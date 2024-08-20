6c6ae71f3cf054918372c5f48a5e73182ff86b9c
Kenneth <kennethchayes@gmail.com>
2024-08-20 09:24:00 -0400

    Restructured layout of code adding a client side and server side

---
+++ client/Navigation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen';
+import MilesScreen from './src/screens/MilesScreen';
+import CaloriesScreen from './src/screens/CaloriesScreen';
+import StepsScreen from './src/screens/StepsScreen';
+
+const Stack = createStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Miles" component={MilesScreen} />
+        <Stack.Screen name="Calories" component={CaloriesScreen} />
+        <Stack.Screen name="Steps" component={StepsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
